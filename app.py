ee0cc74a359784637d8a6e46592720e8af4f0eca
nithinchary23 <ramojipallynithinchary@gmail.com>
2026-03-25 12:47:00 +0530

    Added root route

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def create_app():
     return app
 
 
+
 if __name__ == "__main__":
     app = create_app()
     app.run(
@@ -35,3 +36,7 @@ if __name__ == "__main__":
         host=os.getenv("FLASK_HOST", "127.0.0.1"),
         port=int(os.getenv("FLASK_PORT", "5000"))
     )
+
+@app.route("/")
+def home():
+    return "Backend is running successfully 🚀"
